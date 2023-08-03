c25b35c7dfa3699ec7c8bc03d51008d9b510e0ca
Kem-consolation <consolationlotachi@gmail.com>
2023-08-03 23:23:26 +0100

    Creating reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
 import styled from "styled-components";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
   padding: 20px;
 `;
 const App = () => {
   return (
-    <StyledApp>
-      <H1>The wild Oasis</H1>
-      <Button onClick={() => alert("Check in")}>Check In</Button>
-      <Button onClick={() => alert("Check out")}>Check Out</Button>
-      <Input type="number" placeholder="number of guest" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading type="h1">The wild Oasis</Heading>
+            <div>
+              <Heading type="h2">Check in and Out</Heading>
+              <Button onClick={() => alert("Check in")}>Check In</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="number of guest" />
+              <Input type="number" placeholder="number of guest" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 };
 

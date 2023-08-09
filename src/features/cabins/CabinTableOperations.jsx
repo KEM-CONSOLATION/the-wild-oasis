e14fa270196d852732097b05dcba2e6c09135e88
Kem-consolation <consolationlotachi@gmail.com>
2023-08-09 20:41:45 +0100

    Added the filter function function

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,14 @@
+import Filter from "../../ui/Filter";
+import TableOperations from "../../ui/TableOperations";
+
+const CabinTableOperations = () => {
+  return (
+    <div>
+      <TableOperations>
+        <Filter />
+      </TableOperations>
+    </div>
+  );
+};
+
+export default CabinTableOperations;

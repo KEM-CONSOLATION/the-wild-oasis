3be211b3680fcd17c4660a538591fe1e5d668d20
Kem-consolation <consolationlotachi@gmail.com>
2023-09-11 14:07:03 +0100

    working on the cabin page

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -8,7 +8,6 @@ export async function getBookings({ filter, sortBy }) {
       "id, created_at, startDate, endDate, numNights,numGuests, status, totalPrice, cabins(name), guest(fullName, email)"
     );
 
-  //Filter
   if (filter !== null) query = query.eq(filter.field, filter.value);
   const { data, error } = await query;
 
